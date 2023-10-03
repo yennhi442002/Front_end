70e5e483d34d15a0353f710bb8b690ac4cd01f05
yennhicute44 <hana442002@gmail.com>
2023-10-03 19:24:36 +0700

    my changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,97 @@
 import React from 'react'
-
+import { BsSearch } from "react-icons/bs";
+import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
-    <div>
-      Footerr hungkhang92
-    </div>
-  )
-}
+    <>
+      <footer className='py-4'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-5'>
+              <div className='footer-top-data d-flex gap-30 align-items-center'>
+                <img src="image/newsletter.png" alt="newsletter" />
+                <h2 className='mb-0 text-white'>UPDATE PROMOTION INFORMATION</h2>
+              </div>
+            </div>
+            <div className='col-7'>
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-1"
+                  placeholder="Your email..."
+                  aria-label="Your email..."
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-3 text-white" id="basic-addon2" style={{ backgroundColor: '#ff6600' }}>
+                  Subscribe
+                </span>
 
-export default Footer
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className='py-4'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-4'>
+              <h4 className='text-white mb-4'>Customer Support</h4>
+              <div className='footer-links d-flex flex-column'>
+                <Link className='text-white py-2 mb-1'>Hotline</Link>
+                <Link className='text-white py-2 mb-1'>Frequently Asked Questions</Link>
+                <Link className='text-white py-2 mb-1'>Ordering Guide</Link>
+                <Link className='text-white py-2 mb-1'>Return Policy</Link>
+              </div>
+            </div>
+            <div className='col-3'>
+              <h4 className='text-white mb-4'>About BellaBloom</h4>
+              <div className='footer-links d-flex flex-column'>
+                <Link className='text-white py-2 mb-1'>Introduction BellaBloom</Link>
+                <Link className='text-white py-2 mb-1'>Privacy Policy</Link>
+                <Link className='text-white py-2 mb-1'>Terms Of Use</Link>
+                <Link className='text-white py-2 mb-1'>Contact Us</Link>
+              </div>
+            </div>
+            <div className='col-3'>
+              <h4 className='text-white mb-4'>Account</h4>
+              <div className='footer-links d-flex flex-column'>
+                <Link className='text-white py-2 mb-1'>High-end Cosmetics</Link>
+                <Link className='text-white py-2 mb-1'>Facial Care</Link>
+                <Link className='text-white py-2 mb-1'>Make-Up</Link>
+                <Link className='text-white py-2 mb-1'>Body Care</Link>
+              </div>
+            </div>
+            <div className='col-2'>
+              <h4 className='text-white mb-4'>Categories</h4>
+              <div className='footer-links d-flex flex-column'>
+                <Link className='text-white py-2 mb-1'>High-end cosmetics</Link>
+                <Link className='text-white py-2 mb-1'>Facial Care</Link>
+                <Link className='text-white py-2 mb-1'>Make-Up</Link>
+                <Link className='text-white py-2 mb-1'>Body Care</Link>
+              </div>
+            </div>
+
+
+          </div>
+
+
+        </div>
+      </footer>
+      <footer className='py-4'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12'>
+              <p className='text-center mb-0 text-white'>
+                &copy; {new Date().getFullYear()}; Powered by KhangVo
+              </p>
+            </div>
+          </div>
+        </div>
+
+
+      </footer>
+    </>
+  );
+};
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,74 +2,128 @@ import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { BsSearch } from "react-icons/bs";
 
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 const Header = () => {
   return (
     <>
       <header className="header-top-strip py-3">
-        <div className='container-xxl'>
-          <div className='row'>
-            <div className='col-6'>
-              <p className='text-white mb-0'>FREEESHIP WITH INVOICES FROM 499K</p>
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                FREEESHIP WITH INVOICES FROM 499K
+              </p>
             </div>
-            <div className='col-6'>
-              <p className='text-end text-white mb-0'>
-                Hotline: <a className='text-white mb-0' href="tel: +84 123454678">+84 12345678</a> (Free)
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline:
+                <a className='text-white mb-0' href="tel: +84 123454678">+84 12345678</a> (Free)
+
               </p>
             </div>
           </div>
         </div>
       </header>
-      <header className='header-upper py-3'>
-        <div className='container-xxl'>
-          <div className='row align-items-center'>
-            <div className='col-2'></div>
-            <h2>
-              <Link className='text-white'>Developer.</Link>
-            </h2>
-            <div className='col-5'>
-              <div className="input-group mb-3">
-                <input 
-                  type="text" 
-                  className="form-control py-2" 
-                  placeholder="Search here..." 
-                  aria-label="Search here..." 
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-2">
+              <h2>
+                <Link className="text-white">Cosmectic</Link>
+              </h2>
+            </div>
+            <div className="col-5">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-2"
+                  placeholder="Search Here..."
+                  aria-label="Search Here..."
                   aria-describedby="basic-addon2"
                 />
-                <span className="input-group-text" id="basic-addon2">
+                <span className="input-group-text p-3" id="basic-addon2">
                   <BsSearch></BsSearch>
-                </span> 
+                </span>
               </div>
             </div>
-            <div className='col-5'>
-              <div className='header-upper-links-d-flex justify-content-between'>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
                 <div>
-                  <Link>
-                    <img src="image/compare.svg" alt="compare" />
-                    <p>Compare <br /> Product_Can doi lai
+                  <Link
+                    to="/all-store"
+                    className="d-flex align-items-center gap-10 text-white"
+                  >
+                    <img src="image/store_2.svg" alt="store" />
+                    <p className="mb-0">
+                      All <br /> Store
                     </p>
                   </Link>
                 </div>
                 <div>
-                  <Link>
+                  <Link
+                    to="/wishlist"
+                    className="d-flex align-items-center gap-10 text-white"
+                  >
                     <img src="image/wishlist.svg" alt="wishlist" />
-                    <p>Favourite  <br /> Wishlist
+                    <p className="mb-0">
+                      Favourite <br /> wishlist
                     </p>
                   </Link>
                 </div>
                 <div>
-                  <Link>
+                  <Link
+                    to="/login"
+                    className="d-flex align-items-center gap-10 text-white"
+                  >
                     <img src="image/user.svg" alt="user" />
-                    <p>Login/Register  <br /> My Account
+                    <p className="mb-0">
+                      Log in/Register <br /> My Account
                     </p>
                   </Link>
                 </div>
                 <div>
-                  <Link className='d-flex align-items-center gap'>
+                  <Link
+                    to="/cart"
+                    className="d-flex align-items-center gap-10 text-white"
+                  >
                     <img src="image/cart.svg" alt="cart" />
-                    <div className='d-flex flex-column'>
-                      <span className='badge bg-white text-dark '>0</span>
+                    <div className="d-flex flex-column gap-10">
+                      <span className="badge bg-white text-dark">0</span>
+                      <p className="mb-0">500.000VND</p>
                     </div>
                   </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className='header-bottom py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12'>
+              <div className='menu-bottom d-flex align-items-center gap-30'>
+                <div>
+                  <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      <image src="public/image/wishlist.svg"alt="menu"/> 
+                      <span className='me-5 d-inline-block'>Categories</span>
+                    </button>
+                    <ul className="dropdown-menu">
+                      <li><Link className="dropdown-item" to="" style={{color:'#080808'}}>Action</Link></li>
+                      <li><Link className="dropdown-item" to="" style={{color:'#080808'}}>Another action</Link></li>
+                      <li><Link className="dropdown-item" to="" style={{color:'#080808'}}>Something else here</Link></li>
+                    </ul>
+                  </div>
+                </div>
+                <div className='menu-links'>
+                  <div className='d-flex align-items-center gap-15'>
+                    <NavLink to="/" style={{ color: '#326e51' }}>Home</NavLink>
+                    <NavLink to="/" style={{ color: '#326e51' }}>All Product</NavLink>
+                    <NavLink to="/" style={{ color: '#326e51' }}>Blogs</NavLink>
+                    <NavLink to="/" style={{ color: '#326e51' }}>Contact Us</NavLink>
+                  </div>
                 </div>
               </div>
             </div>
